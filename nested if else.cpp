260c0f3ad4db19ac85a1276282602cba842d6c32
Rashmi kumari <102369387+Rashmi6203@users.noreply.github.com>
2023-01-06 13:33:57 +0530

    Add files via upload

---
+++ nested if else.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	printf("Enter either 1 or 2 :");
+	scanf("%d",&i);
+	
+	if (i==1)
+	
+		printf("you would go to heaven !\n");
+		else
+		{
+			if(i==2)
+			printf("hell was created with you in mind ! \n");
+			else
+			
+				printf("how about mother earth !\n");
+			
+		}
+	
+}
